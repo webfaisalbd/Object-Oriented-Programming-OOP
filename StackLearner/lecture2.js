836ed877b8fca9657e8cf20a322ae9c1e6c739ff
webfaisalbd <web.faisal.bd@gmail.com>
2022-08-27 23:47:13 +0600

    run loop in all object

--- StackLearner/lecture2.js
+++ StackLearner/lecture2.js
@@ -18,5 +18,15 @@ function Person(name, email) {
 
 // var p1 = Person();
 var p1 = new Person("faisal", 'faisal@gmail.com');
+var p2 = new Person("rasel", 'rasel@gmail.com');
+var p3 = new Person("jony", 'jony@gmail.com');
 // console.log(p1.name);
-console.log(p1);
+// console.log(p1);
+
+let people = [p1, p2, p3];
+
+people.forEach(person => {
+   console.log(person.email); 
+});
+
+
