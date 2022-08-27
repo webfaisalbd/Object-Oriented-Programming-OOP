385a37d62de9837e89f1dea67b1bc997ead066a8
webfaisalbd <web.faisal.bd@gmail.com>
2022-08-27 16:14:26 +0600

    key:value pair and method call added

--- StackLearner/lecture1.js
+++ StackLearner/lecture1.js
@@ -1,1 +1,27 @@
-console.log("Lecture 1 started:");
+// object: jar ekadik property thakbe.
+// example:  user ekta object.
+// user object er property, like:
+// userName, userReview
+// ekhon AbstractRange, userReview ekta object hote parent, 
+// Jemon, reviewWriter, reviewTime etc. 
+
+// object creation 
+// var obj = {};
+// var obj1 = new Object;
+// console.log(obj);
+// console.log(obj1);
+
+var book = {
+    // javascript e object mane=> key:value pair
+    // key: value , 
+    name: "functional javascript",
+    author: "faisal",
+    publisher: 'o\'Adarsha',
+    page: 250,
+
+    // object er vitore function declare korle, seta k bole: method
+    print: function(){
+        console.log(this.name, this.author);
+    }
+}
+book.print(); // method call 
