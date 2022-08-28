0118801baf071244a4d62f775aa4998043813670
webfaisalbd <web.faisal.bd@gmail.com>
2022-08-29 00:15:27 +0600

    make prototype as object

--- README.md
+++ README.md
@@ -398,7 +398,26 @@ p2.print(); // Farhan 37 faq@gmail.com
 
 
 ```javascript
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
 
+Person.prototype = {
+    hello: function(){
+        console.log("hello, "+ this.name);
+    },
+    print: function(){
+        console.log(this.name, this.age, this.email);
+    },
+    email: "faq@gmail.com"
+}
+
+let p1 = new Person("Faisal", 27);
+let p2 = new Person("Farhan", 37);
+
+console.log(p1);
+console.log(p2);
 ```
 
 
--- StackLearner/lecture4.js
+++ StackLearner/lecture4.js
@@ -13,19 +13,29 @@ function Person(name, age){
     this.age = age;
 }
 
-Person.prototype.hello = function(){
-    console.log("hello "+ this.name);
-}
+// Person.prototype.hello = function(){
+//     console.log("hello "+ this.name);
+// }
 
-Person.prototype.email = "faq@gmail.com";
+// Person.prototype.email = "faq@gmail.com";
 
-Person.prototype.print = function(){
-    console.log(this.name, this.age, this.email);
+// Person.prototype.print = function(){
+//     console.log(this.name, this.age, this.email);
+// }
+
+Person.prototype = {
+    hello: function(){
+        console.log("hello, "+ this.name);
+    },
+    print: function(){
+        console.log(this.name, this.age, this.email);
+    },
+    email: "faq@gmail.com"
 }
 
 let p1 = new Person("Faisal", 27);
 let p2 = new Person("Farhan", 37);
 
-p1.print(); // Faisal 27 faq@gmail.com
-p2.print(); // Farhan 37 faq@gmail.com
+console.log(p1);
+console.log(p2);
 
