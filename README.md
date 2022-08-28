a87d710351c79b9a40166af239671d1b3b84807c
webfaisalbd <web.faisal.bd@gmail.com>
2022-08-29 00:01:45 +0600

    using prototype

--- README.md
+++ README.md
@@ -326,11 +326,25 @@ console.log(p1);
 console.log(p2);
 ```
 
-
-
-
+- using prototype
 ```javascript
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.hello = function(){
+    console.log("hello "+ this.name);
+}
+// console.log(Person.prototype);
+// prototype ekta object, tar vitore function ase.
 
+let p1 = new Person("Faisal", 27);
+let p2 = new Person("Farhan", 37);
+
+console.log(p1);
+console.log(p2);
+
+// Jokhon object create kora hobe, tokhon object er sathe __proto__ name er extra ekta property add hoye jabe.
 ```
 
 
--- StackLearner/lecture4.js
+++ StackLearner/lecture4.js
@@ -1,15 +1,30 @@
 // code duplication, without prototype
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+
+//     this.hello = function(){
+//         console.log("hello"+ this.name);
+//     }
+// }
+
 function Person(name, age){
     this.name = name;
     this.age = age;
-
-    this.hello = function(){
-        console.log("hello"+ this.name);
-    }
 }
+
+Person.prototype.hello = function(){
+    console.log("hello "+ this.name);
+}
+
+// console.log(Person.prototype);
+// prototype ekta object, tar vitore function ase.
+
 
 let p1 = new Person("Faisal", 27);
 let p2 = new Person("Farhan", 37);
 
 console.log(p1);
 console.log(p2);
+
+// Jokhon object create kora hobe, tokhon object er sathe __proto__ name er extra ekta property add hoye jabe.
