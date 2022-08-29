39fbc81e9992691605f5de70f92af0bd7cfd3e4b
webfaisalbd <web.faisal.bd@gmail.com>
2022-08-29 19:26:18 +0600

    constructor function

--- README.md
+++ README.md
@@ -716,15 +716,41 @@ let obj3 = new Obj3();
 ```
 
 
-
+`constructor`
 ```javascript
+const sumit = {
+    firstName: "sumit",
+    age: 35,
+    print: function(){
+        return this.firstName + " and my age is: " +this.age;
+    }
+}
 
+function Person(first, last, age){ // constructor function
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.fullName = function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+const faisal = new Person("faisal","ahmed", 26);
+const farhad = new Person("forid", "hossain", 27);
+console.log(faisal.fullName());
 ```
 
 
 
 ```javascript
+// object dot(.) something = possible, mane, object er sathe new property add kora jabe
+faisal.country ="Bangladesh"; // faisal is an object
+// console.log(faisal);
 
+// constructor function dot(.) something = possible na, mane, constructor function er sathe new property add kora jabe na. 
+// constructor function ekta template, oita fixed e thakbe.
+Person.country = "Bangladesh"; // // Person is a constructor function
+console.log(Person);
 ```
 
 
--- StackLearner/lecture8.js
+++ StackLearner/lecture8.js
@@ -1,8 +1,8 @@
 // constructor pattern er maddhome object create
-function Obj(a, b) {
-    this.a = a;
-    this.b = b;
-}
+// function Obj(a, b) {
+//     this.a = a;
+//     this.b = b;
+// }
 // let obj1 = new Obj(1,2);
 
 // browser console e.
@@ -16,19 +16,58 @@ function Obj(a, b) {
 // Obj.prototype = {}; // eikhane constructor ta overwrite kore {} diye dise.
 
 
-function Obj2(a,b,c){
-Obj.call(this, a,b);
-this.c = c;
-}
+// function Obj2(a,b,c){
+// Obj.call(this, a,b);
+// this.c = c;
+// }
 
-Obj2.prototype = Object.create(Obj.prototype);
+// Obj2.prototype = Object.create(Obj.prototype);
 
-function Obj3(){
-Obj2.call(this, 10,15,20);
-}
+// function Obj3(){
+// Obj2.call(this, 10,15,20);
+// }
 
-Obj3.prototype = Object.create(Obj2.prototype);
+// Obj3.prototype = Object.create(Obj2.prototype);
 
 // let obj1 = new Obj(1,2);
 // let obj2 = new Obj2(4,5,6);
-let obj3 = new Obj3();
+// let obj3 = new Obj3();
+
+
+
+// ------------------------------------------
+// ------------------------------------------
+
+const sumit = {
+    firstName: "sumit",
+    age: 35,
+    print: function(){
+        return this.firstName + " and my age is: " +this.age;
+    }
+}
+
+function Person(first, last, age){ // constructor function
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.fullName = function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+const faisal = new Person("faisal","ahmed", 26);
+const farhad = new Person("forid", "hossain", 27);
+// console.log(faisal.fullName());
+
+// object dot(.) something = possible, mane, object er sathe new property add kora jabe
+faisal.country ="Bangladesh";
+// console.log(faisal);
+
+// constructor function dot(.) something = possible na, mane, constructor function er sathe new property add kora jabe na. 
+// constructor function ekta template, oita fixed e thakbe.
+Person.country = "Bangladesh";
+console.log(Person);
+
+
+
+
