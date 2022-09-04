e92f938069171b41c99db565125571fc61300d0d
webfaisalbd <web.faisal.bd@gmail.com>
2022-09-04 20:44:37 +0600

    object er khetre this 4

--- OOP-Concepts/lecture9.js
+++ OOP-Concepts/lecture9.js
@@ -2,14 +2,20 @@ var myObj = {
     name: "faisal",
     getDetails: function(){
         let name= "ahmed";
-         console.log(this.name); // faisal
     },
     anotherObj : {
         name : "shajib",
         value : function() {
-            console.log(this.name);
+            return moreObj = {
+                name: "fas",
+                moreFunc: () => {
+                    name: "hard work"
+                    console.log(this.name);
+                }
+            }
         }
     } 
 }
-myObj.anotherObj.value();
+myObj.anotherObj.value().moreFunc(); // shajib
+
 
--- README.md
+++ README.md
@@ -358,6 +358,25 @@ myObj.anotherObj.value();
 
 - code 4
 ```javascript
+var myObj = {
+    name: "faisal",
+    getDetails: function(){
+        let name= "ahmed";
+    },
+    anotherObj : {
+        name : "shajib",
+        value : function() {
+            return moreObj = {
+                name: "fas",
+                moreFunc: () => {
+                    name: "hard work"
+                    console.log(this.name);
+                }
+            }
+        }
+    } 
+}
+myObj.anotherObj.value().moreFunc(); // shajib
 
 ```
 
