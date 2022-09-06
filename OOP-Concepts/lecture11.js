bdb064dcf7421c7bf016f1e20817a798640edc21
webfaisalbd <web.faisal.bd@gmail.com>
2022-09-06 23:15:05 +0600

    object propeorty repeated problem solved by using function constructor

--- OOP-Concepts/lecture11.js
+++ OOP-Concepts/lecture11.js
@@ -1,15 +1,32 @@
-// normal object problem 
-var samir = {
-    name: "Samir",
-    age: 28,
+// // normal object problem 
+// var samir = {
+//     name: "Samir",
+//     age: 28,
+// }
+
+// var jamir = {
+//     name: "Jamir",
+//     age: 35
+// }
+
+// console.log(samir.name); // samir
+// console.log(jamir.name); // jamir
+
+// // here, dui ta object er e property same, but bar bar property likha lagtese
+
+
+
+
+
+// object er same repeated property, problem solved below:
+// making constructor function as a blue print 
+var Person = function(name, age){
+    this.name= name;
+    this.age = age;
 }
 
-var jamir = {
-    name: "Jamir",
-    age: 35
-}
+var faisal = new Person("Faisal Ahmed",26);
+var rifat = new Person("Rifat", 25);
 
-console.log(samir.name); // samir
-console.log(jamir.name); // jamir
-
-// here, dui ta object er e property same, but bar bar property likha lagtese
+console.log(faisal.name); // Faisal Ahmed
+console.log(rifat.name); // Rifat
--- README.md
+++ README.md
@@ -509,10 +509,21 @@ console.log(jamir.name); // jamir
 // here, dui ta object er e property same, but bar bar property likha lagtese
 ```
 
-
+- object er same repeated property, problem solved below:
+- making constructor function as a blue print
 ```javascript
+var Person = function(name, age){
+    this.name= name;
+    this.age = age;
+}
 
+var faisal = new Person("Faisal Ahmed",26);
+var rifat = new Person("Rifat", 25);
+
+console.log(faisal.name); // Faisal Ahmed
+console.log(rifat.name); // Rifat
 ```
+
 
 
 ```javascript
